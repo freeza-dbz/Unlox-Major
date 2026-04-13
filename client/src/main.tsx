6875fb688efbcfe3149278ae759446e552136bdd
Anmol Singh <anmol2006.chauhan@gmail.com>
2026-04-13 18:40:23 +0530

    bug fixing

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,15 +2,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import { AuthProvider } from './contexts/AuthContext.tsx';
-import { ThemeProvider } from './contexts/ThemeContext.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider>
       <AuthProvider>
         <App />
       </AuthProvider>
-    </ThemeProvider>
   </StrictMode>
 );
